2ee164ce8f88cc07c68a1b94e2857cc4a0d13447
deepak <admin@Admins-MacBook-Pro.local>
2021-06-07 21:40:41 +0800

    Springoot initial project from start.spring.io

---
+++ src/main/java/com/mycompany/api/ebankingPortal/EbankingPortalApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.api.ebankingPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbankingPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbankingPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompany/api/ebankingPortal/EbankingPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.api.ebankingPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbankingPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
