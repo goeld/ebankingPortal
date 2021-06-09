8699ed6a3ace4e22ffbcbf2c8f262997a5bfc28c
deepak <admin@Admins-MacBook-Pro.local>
2021-06-09 23:52:43 +0800

    Enable swagger

--- src/main/java/com/mycompany/api/ebankingPortal/EbankingPortalApplication.java
+++ src/main/java/com/mycompany/api/ebankingPortal/EbankingPortalApplication.java
@@ -1,9 +1,12 @@
 package com.mycompany.api.ebankingPortal;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "eBanking Portal API", version = "0.0.1", description = "eBanking API for customer account transactions"))
 public class EbankingPortalApplication {
 
 	public static void main(String[] args) {
